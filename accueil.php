83dc8f0a7faad9777a2afaead66d5b1824908b54
Hugi Romane <romane.h@codeur.online>
2017-02-13 14:48:07 +0100

    Mise en place page

---
+++ accueil.php
@@ -0,0 +1,67 @@
+<?php 
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+?>
+
+
+<!DOCTYPE html>
+
+
+  <html>
+
+  	<head>
+
+          <meta charset="utf-8" />
+          <title>Générateur avatar acs</title>
+          <meta name="description" content="Bootstrap" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+          <link rel="stylesheet" type="text/css" href="style_accueil.css"/>
+          <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="bootstrap/bootstrap.min.js"></script>
+
+
+  	</head>
+  	
+    <body>
+
+
+      <header>
+
+        <h1>Nom du générateur</h1>
+
+      </header>
+ 
+        <section id="avatar"> 
+
+          <div class="container">
+
+            <div class="row">
+
+              <div class="col-xs-12 col-md-6">
+
+                <div class="apercu"></div>
+
+              </div>
+
+              <div class="col-xs-12 col-md-6">
+
+                <div class="choix"></div>
+
+              </div>
+
+        </section>
+
+    </body>
+
+  </html>
+
+
+
+
+
+
+
+
+
