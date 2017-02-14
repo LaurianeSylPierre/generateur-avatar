e726ef41dfb9d0f4e5c057efd72af1daffd431fa
Unknown <eddyroy2125@gmail.com>
2017-02-14 15:47:35 +0100

    Ajout fichier ajax.php

---
+++ Controller/ajax.php
@@ -0,0 +1,6 @@
+<?php 
+require '../Model/pdo.php';
+$save = str_replace('data:image/png;base64,', '', $_POST['image'] );
+file_put_contents( '../View/Images/'.$_POST['pseudo'].'.png', base64_decode( $save ) );
+$query = $pdo->query("INSERT INTO Avatars (pseudo) VALUES ('".$_POST['pseudo'].".png')");
+?>
