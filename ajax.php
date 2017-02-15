6e0abe95c883e54b69a676b665a84a3a08b78949
Unknown <eddyroy2125@gmail.com>
2017-02-15 16:17:03 +0100

    Affichage avatar créés dans l'accueil, modification ajax

--- ajax.php
+++ ajax.php
@@ -2,5 +2,5 @@
 require_once 'Model/pdo.php';
 $save = str_replace('data:image/png;base64,', '', $_POST['image'] );
 file_put_contents( 'Views/Images/'.$_POST['pseudo'].'.png', base64_decode($save));
-$query = $pdo->query("INSERT INTO Avatars (pseudo) VALUES ('".$_POST['pseudo'].".png')");
+$query = $pdo->query("INSERT INTO Avatars (url_home,image,pseudo) VALUES ('".$base_url.",".$_POST['pseudo'].".png','".$_POST['pseudo']."')");
 ?>
