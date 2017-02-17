d108ab5df25518807c8884621b312cfe2eed6d76
Unknown <laurianepierre@outlook.fr>
2017-02-17 10:49:14 +0100

    oops

--- editeur.php
+++ editeur.php
@@ -29,6 +29,35 @@ require_once "Model/pdo.php";
   	</head>
 
     <body>
+        <div id="fb-root"></div>
+        <script>
+        // Widget facebook
+        // (function(d, s, id) {
+        //     var js, fjs = d.getElementsByTagName(s)[0];
+        //     if (d.getElementById(id)) return;
+        //     js = d.createElement(s); js.id = id;
+        //     js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.8";
+        //     fjs.parentNode.insertBefore(js, fjs);
+        // }(document, 'script', 'facebook-jssdk'));
+
+        // widget twitter
+        // window.twttr = (function(d, s, id) {
+        //     var js, fjs = d.getElementsByTagName(s)[0],
+        //         t = window.twttr || {};
+        //     if (d.getElementById(id)) return t;
+        //     js = d.createElement(s);
+        //     js.id = id;
+        //     js.src = "https://platform.twitter.com/widgets.js";
+        //     fjs.parentNode.insertBefore(js, fjs);
+        //
+        //     t._e = [];
+        //     t.ready = function(f) {
+        //         t._e.push(f);
+        //     };
+        //
+        //     return t;
+        // }(document, "script", "twitter-wjs"));
+        </script>
 
       <header>
 
@@ -196,8 +225,6 @@ require_once "Model/pdo.php";
 								});
 							}
 						});
-					});
-				});
 	        </script>
 
         </section>
